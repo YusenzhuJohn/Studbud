aae2d836216b19c09970091247c6cc68796e4ecc
Yusen Zhu <yzhu4495@uni.sydney.edu.au>
2022-06-06 13:54:54 +0800

    tasklist

--- dist/index.f3bd186e.js
+++ dist/index.f3bd186e.js
@@ -1,55 +1,114 @@
-const form = document.getElementById("taskform");
-const button = document.querySelector("#taskform > button");
-var taskInput = document.getElementById("taskInput");
-var tasklist = document.getElementById("tasklist");
-var dueDateInput = document.getElementById("dueDateInput");
-var completionTimeInput = document.getElementById("completionTimeInput");
-var estimatedTimeInput = document.getElementById("estimatedTimeInput");
-var priorityInput = document.getElementById("priorityInput");
-form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    let task = taskInput.value;
-    let dueDate = dueDateInput.value;
-    let completionTime = completionTimeInput.value;
-    let estimatedTime = estimatedTimeInput.value;
-    let priorityRating = priorityInput.options[priorityInput.selectedIndex].value;
-    addTask(task, dueDate, estimatedTime, priorityRating, completionTime, false);
-    console.log(taskList);
+// board js
+const todos = document.querySelectorAll(".todo");
+const all_status = document.querySelectorAll(".status");
+let draggableTodo = null;
+todos.forEach((todo)=>{
+    todo.addEventListener("dragstart", dragStart);
+    todo.addEventListener("dragend", dragEnd);
 });
-var taskListArray = [];
-function addTask(taskDescription, dueDate, estimatedTime, priorityRating, completionTime, completionStatus) {
-    let d = new Date();
-    let dateCreated = d.getFullYear();
-    let task = {
-        taskDescription,
-        dueDate,
-        dateCreated,
-        estimatedTime,
-        completionTime,
-        priorityRating,
-        estimatedTime,
-        completionStatus
-    };
-    taskListArray.push(task);
-    renderTask(task);
+function dragStart() {
+    draggableTodo = this;
+    setTimeout(()=>{
+        this.style.display = "none";
+    }, 0);
+    console.log("dragStart");
 }
-function renderTask(task) {
-    // Create HTML elements
-    let item = document.createElement("li");
-    item.innerHTML = "<p>" + task.taskDescription + "</p>";
-    tasklist.appendChild(item);
-    // Extra Task DOM elements
-    let delButton = document.createElement("button");
-    let delButtonText = document.createTextNode("Delete Task");
-    delButton.appendChild(delButtonText);
-    item.appendChild(delButton);
-    // Event Listeners for DOM elements
-    delButton.addEventListener("click", function(event) {
-        event.preventDefault();
-        item.remove();
+function dragEnd() {
+    draggableTodo = null;
+    setTimeout(()=>{
+        this.style.display = "block";
+    }, 0);
+    console.log("dragEnd");
+}
+all_status.forEach((status)=>{
+    status.addEventListener("dragover", dragOver);
+    status.addEventListener("dragenter", dragEnter);
+    status.addEventListener("dragleave", dragLeave);
+    status.addEventListener("drop", dragDrop);
+});
+function dragOver(e) {
+    e.preventDefault();
+//   console.log("dragOver");
+}
+function dragEnter() {
+    this.style.border = "1px dashed #ccc";
+    console.log("dragEnter");
+}
+function dragLeave() {
+    this.style.border = "none";
+    console.log("dragLeave");
+}
+function dragDrop() {
+    this.style.border = "none";
+    this.appendChild(draggableTodo);
+    console.log("dropped");
+}
+/* modal */ const btns = document.querySelectorAll("[data-target-modal]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+btns.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+        document.querySelector(btn.dataset.targetModal).classList.add("active");
+        overlay.classList.add("active");
     });
-    // Clear the input form
-    form.reset();
+});
+close_modals.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+        const modal = btn.closest(".modal");
+        modal.classList.remove("active");
+        overlay.classList.remove("active");
+    });
+});
+window.onclick = (event)=>{
+    if (event.target == overlay) {
+        const modals = document.querySelectorAll(".modal");
+        modals.forEach((modal)=>modal.classList.remove("active"));
+        overlay.classList.remove("active");
+    }
+};
+/* create todo  */ const todo_submit = document.getElementById("todo_submit");
+todo_submit.addEventListener("click", createTodo);
+function createTodo() {
+    const todo_div = document.createElement("div");
+    const input_val = document.getElementById("todo_input").value;
+    const input_val2 = document.getElementById("duedate_input").value;
+    const input_val3 = document.getElementById("completionTime_input").value;
+    const input_val4 = document.getElementById("estimatedTime_input").value;
+    const input_val5 = document.getElementById("priority_input").value;
+    const txt = document.createTextNode(input_val);
+    const dte = document.createTextNode(input_val2);
+    const ct = document.createTextNode(input_val3);
+    const et = document.createTextNode(input_val4);
+    const pr = document.createTextNode(input_val5);
+    todo_div.appendChild(txt);
+    todo_div.appendChild(dte);
+    todo_div.appendChild(ct);
+    todo_div.appendChild(et);
+    todo_div.appendChild(pr);
+    console.log(todo_div);
+    todo_div.classList.add("todo");
+    todo_div.setAttribute("draggable", "true");
+    /* create span */ const span = document.createElement("span");
+    const span_txt = document.createTextNode("\xd7");
+    span.classList.add("close");
+    span.appendChild(span_txt);
+    todo_div.appendChild(span);
+    no_status.appendChild(todo_div);
+    span.addEventListener("click", ()=>{
+        span.parentElement.style.display = "none";
+    });
+    //   console.log(todo_div);
+    todo_div.addEventListener("dragstart", dragStart);
+    todo_div.addEventListener("dragend", dragEnd);
+    document.getElementById("todo_input").value = "";
+    todo_form.classList.remove("active");
+    overlay.classList.remove("active");
 }
+const close_btns = document.querySelectorAll(".close");
+close_btns.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+        btn.parentElement.style.display = "none";
+    });
+});
 
 //# sourceMappingURL=index.f3bd186e.js.map
